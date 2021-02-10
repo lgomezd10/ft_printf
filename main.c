05a4db189065d326af62b5796d6713fe352f67eb
lgomezd10 <lgomezd10@gmail.com>
2021-02-10 09:43:08 +0100

    mas pruebas

--- main.c
+++ main.c
@@ -30,8 +30,23 @@ void foo(char *fmt, ...)
 
 int main(void)
 {
+    //cspdiuxX % x y X conversion hexadecimal sin signo
+    printf("un hexadecima %x otro %x\n", -2147483647 , -2);
+    printf("un decimal sin signo %u otro %u\n", 5, -1);
+
     printf("una cosa %9s otra cosa\n", "prueba");
     printf("una cosa %-9s otra cosa\n", "prueba");
     printf("una cosa %3s otra cosa\n", "prueba");
+
+    printf("prueba del punto %.9s\n", "unomuylargoparaque recorte");
+    printf("prueba del digito %9d\n", 30);
+    printf("prueba del digito %.*d\n", 2, 305456);
+    printf("prueba del digito con menos %-9d hola\n", 30);
+    //printf("prueba del digito %-09d\n", 30); // el menos no funciona con cero
+    printf("El punto con digito rellena de ceros %0.9d\n", 30); //o lo usar para rellenar de ceros o lo ignora
+    printf("prueba del punto y * %.*s\n", 5, "unomuylargoparaque recorte");
+    printf("prueba del 0 %09d\n", 30); //solo para numeros
+    printf("prueba de * %*s\n", 9, "uno");
+    printf("prueba de * %-*s dos\n", 9, "uno");
     //    foo("ss", "hola", "hola tu");
 }
